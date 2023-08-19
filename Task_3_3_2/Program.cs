3e81acb19b1b1d6af2c7a06991b516fb8ebc3f4b
Anton <anton.batalov94@gmail.com>
2023-08-19 14:07:13 +0700

    Task 3.3.2- done

---
+++ Task_3_3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName = "Антон";
+        byte MyAge = 28;
+        bool iHaveAPet = true;
+        float MyShoeSize = 44;
+        Console.WriteLine("My name is " + myName);
+        Console.WriteLine("My age is " + MyAge);
+        Console.WriteLine("Do I have a pet? " + iHaveAPet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+        Console.ReadKey();
+
+    }
+}
