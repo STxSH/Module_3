e55d8af8ec7ff35769bfa77575c70abba127bd70
Anton <anton.batalov94@gmail.com>
2023-08-19 15:08:56 +0700

    task 3.4.3 - done

---
+++ Task_3_4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Schema;
+
+namespace Task_3_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        enum semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+    }
+}
