3cdeb56427c3c2c8f993cb88cfe9b110736a64f8
Anton <anton.batalov94@gmail.com>
2023-08-19 13:51:47 +0700

    Добавьте файлы проекта.

---
+++ Task_3_2_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        const string MyName = "Антон";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 28 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine('\x23');
+        Console.WriteLine((char)35);
+        Console.ReadKey();
+
+    }
+}
